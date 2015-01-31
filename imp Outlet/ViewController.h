1161b59e503a6fa844ad2a5ba1813e5ac8cc97c7
viccarre <viccarre@gmail.com>
2015-01-31 03:02:33 -0500

    Switches sending URL Req to Imp

--- imp Outlet/ViewController.h
+++ imp Outlet/ViewController.h
@@ -10,6 +10,10 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)switch1:(id)sender;
+@property (strong, nonatomic) IBOutlet UITextField *urlOne;
+- (IBAction)switch2:(id)sender;
+- (IBAction)switch3:(id)sender;
 
 @end
 
--- imp Outlet/ViewController.m
+++ imp Outlet/ViewController.m
@@ -16,12 +16,75 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+    label.backgroundColor = [UIColor clearColor];
+    label.font = [UIFont fontWithName:@"Avenir Next Medium" size:24.0];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor whiteColor]; // change this color
+    self.navigationItem.titleView = label;
+    label.text = NSLocalizedString(@"Imp Outlet", @"");
+    [label sizeToFit];
+    [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:241.0/255.0 green:196.0/255.0 blue:15.0/255.0 alpha:1]];
+    [self.navigationController.navigationBar setTranslucent:NO];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)switch1:(id)sender {
+    
+    if ([sender isOn]) {
+        NSString *agentURL = _urlOne.text;
+        NSURL *url = [NSURL URLWithString:[agentURL stringByAppendingString:@"?out1=1"]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"ret=%@", ret);
+        
+    } else {
+        NSString *agentURL = _urlOne.text;
+        NSURL *url = [NSURL URLWithString:[agentURL stringByAppendingString:@"?out1=0"]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"ret=%@", ret);
+    }
+    
+}
+- (IBAction)switch2:(id)sender {
+    
+    if ([sender isOn]) {
+        NSString *agentURL = _urlOne.text;
+        NSURL *url = [NSURL URLWithString:[agentURL stringByAppendingString:@"?out2=1"]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"ret=%@", ret);
+        
+    } else {
+        NSString *agentURL = _urlOne.text;
+        NSURL *url = [NSURL URLWithString:[agentURL stringByAppendingString:@"?out2=0"]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"ret=%@", ret);
+    }
+}
+
+- (IBAction)switch3:(id)sender {
+    
+    if ([sender isOn]) {
+        NSString *agentURL = _urlOne.text;
+        NSURL *url = [NSURL URLWithString:[agentURL stringByAppendingString:@"?out3=1"]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"ret=%@", ret);
+        
+    } else {
+        NSString *agentURL = _urlOne.text;
+        NSURL *url = [NSURL URLWithString:[agentURL stringByAppendingString:@"?out3=0"]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"ret=%@", ret);
+    }
+    
+}
 @end
